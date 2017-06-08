311f97886d65e0ffe6b5a54adeb1829de6b0b97f
root <root@localhost.localdomain>
2017-06-08 13:04:08 +0100

    popo

--- pygame1.py
+++ pygame1.py
@@ -12,9 +12,13 @@ car_height = 54
 
 black = (0,0,0)
 white = (255,255,255)
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
+red = (200,0,0)
+green = (0,200,0)
+blue = (0,0,200)
+bright_red = (250,0,0)
+bright_green = (0,250,0)
+bright_blue = (0,0,250)
+
 
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption("A bit Racey")
@@ -45,11 +49,8 @@ def message_display(text):
     TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = ((display_width/2), (display_height/2))
     gameDisplay.blit(TextSurf, TextRect)
-
     pygame.display.update()
-
     time.sleep(2)
-
     game_loop()
 
 def game_logic():
@@ -104,6 +105,50 @@ def game_init(x,y,x_change,dodged,thing_startx,thing_starty,thing_speed,thing_wi
     thing_speed = 7
     thing_width = 100
     thing_height = 100
+
+def game_quit():
+    pygame.quit()
+    quit()
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x<mouse[0]<x+w and y<mouse[1]<y+h:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.Font("freesansbold.ttf", 20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ((x+(w/2)), (y+(h/2)))
+    gameDisplay.blit(textSurf, textRect)
+
+
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            #print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font("freesansbold.ttf",115)
+        TextSurf, TextRect = text_objects("A bit Racey", largeText)
+        TextRect.center = ((display_width/2), (display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("GO!",150,450,100,50,green,bright_green,game_loop)
+        button("Quit",500,450,100,50,red,bright_red,game_quit)
+
+        pygame.display.update()
+        clock.tick(15)
 
 def game_loop():
 
@@ -173,6 +218,7 @@ def game_loop():
 
         clock.tick(60)
 
+game_intro()
 game_loop()
 pygame.quit()
 quit()
